9ebd217f785733fbbdbbed49c483af77cbc1f357
斯黄 <cyx20110508@163.com>
2024-07-17 09:13:22 +0800

    Fixed the issue where IP queries failed due to the inability to use Google DNS services in Chinese Mainland.

--- src/latency.js
+++ src/latency.js
@@ -1,9 +1,21 @@
 const ping = require('ping');
 
-// 测量单个URL的延迟
+// 测量单个 URL 的延迟
 async function measureLatency(url) {
-    const res = await ping.promise.probe(url);
-    return res.time;
+    try {
+        // 使用 ping.promise.probe 方法测量延迟
+        const res = await ping.promise.probe(url);
+
+        // 如果 ping 成功返回延迟时间，否则返回 'timeout'
+        if (res.alive) {
+            return res.time;
+        } else {
+            return 'timeout';
+        }
+    } catch (error) {
+        console.error(`Error while measuring latency for ${url}: ${error.message}`);
+        return 'timeout'; // 出现错误时返回 'timeout'
+    }
 }
 
 module.exports = measureLatency;
