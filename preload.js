c63338673f80866e348c6e1575128ba320534faf
斯黄 <cyx20110508@163.com>
2024-07-24 22:48:23 +0800

    Updated open-source license to GPLv3.

--- preload.js
+++ preload.js
@@ -1,3 +1,22 @@
+/*
+  MultiSite Latency Tool
+
+  Copyright (C) 2024 Snake Konginchrist
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 // 预加载脚本，用于在渲染器进程中使用Node.js模块
 const { contextBridge, ipcRenderer } = require('electron');
 
